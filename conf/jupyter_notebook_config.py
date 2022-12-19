1139825aca141e136a0fe7005561fec8b25940af
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-19 14:27:59 -0800

    monorepo: use submodule

--- conf/jupyter_notebook_config.py
+++ conf/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.9
 c.ServerApp.contents_manager_class = "jupytext.TextFileContentsManager"
 c.ContentsManager.notebook_extensions = "ipynb,Rmd,jl,md,py"
-c.ServerApp.root_dir = "./notebooks"
-c.ServerApp.notebook_dir = "./notebooks"
+c.ServerApp.root_dir = "./playground"
+c.ServerApp.notebook_dir = "./playground"
