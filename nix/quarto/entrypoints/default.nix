eb6095d4cf33c89e5eca1f5db1174e4c75c7307e
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-22 03:08:40 -0800

    docs: publish quarto

--- nix/quarto/entrypoints/default.nix
+++ nix/quarto/entrypoints/default.nix
@@ -6,17 +6,6 @@
   inherit (inputs.cells-lab.writers.lib) writeShellApplication;
 in {
   example = cell.lib.mkEnv {
-    r = ps:
-      with ps; [
-        # add your custom R packages here
-        ggplot2
-        dplyr
-        ggplot2
-        lubridate
-        readr
-        ggrepel
-        tidyr
-      ];
     python = ps:
       with ps; [
         # add your custom Python packages here
@@ -26,15 +15,16 @@ in {
     runtimeInputs = with nixpkgs; [sd];
     text = ''
       # write your custom bash script here
-      tmp="$PRJ_ROOT/.cache/"
+      dir="$PRJ_ROOT/docs/"
+
       path=("$@")
       # shellcheck disable=SC2128
       file="$(basename "$path")"
-      cp "$@" "$tmp"/"''${file%.md}".qmd
-      sd './attach' "$PRJ_ROOT/docs/publish/static/ox-hugo" "$tmp""''${file%.md}".qmd
-      sd '```julia\n\#\|' '```{julia}\n#|' "$tmp""''${file%.md}".qmd
+      cp "$@" "$dir"/"''${file%.md}".qmd
+      sd './attach' "$PRJ_ROOT/docs/publish/static/ox-hugo" "$dir""''${file%.md}".qmd
+      sd '```julia\n\#\|' '```{julia}\n#|' "$dir""''${file%.md}".qmd
 
-      quarto render  "$tmp""''${file%.md}".qmd --to html
+      quarto render  "$dir""''${file%.md}".qmd --to html
     '';
   };
 }
