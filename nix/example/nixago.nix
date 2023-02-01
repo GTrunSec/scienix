2e6cfdd58723ea4c2bb49be05caa16eaf33b8eea
guangtao <gtrunsec@hardenedlinux.org>
2023-01-31 22:57:00 -0800

    GPT3-gitcmmit: Update Nginx configuration for Docker Compose
    - Update Nginx configuration for Docker Compose
    - Upgrade Nginx version to `1.19.2`
    - Add port and volume mapping for Nginx configuration file
    
    [nix/example/nixago.nix]
    - Add Nginx configuration for Docker Compose
    - Update version of Nginx to `1.19.2`
    - Add port mapping for Nginx
    - Add volume mapping for Nginx configuration file

--- nix/example/nixago.nix
+++ nix/example/nixago.nix
@@ -9,4 +9,19 @@ builtins.mapAttrs (_: inputs.std.lib.dev.mkNixago) {
     format = "yaml";
     hook.mode = "copy";
   };
+  nginx = {
+    configData = {
+      version = "3.7";
+      services = {
+        nginx = {
+          image = "nginx:1.19.2";
+          ports = [ "80:80" ];
+          volumes = [ "./modules/infra/hello/nginx.conf:/etc/nginx/conf.d/default.conf" ];
+        };
+      };
+    };
+    output = "modules/infra/nginx/docker-compose.yaml";
+    format = "yaml";
+    hook.mode = "copy";
+  };
 }
