dccfbb4a588cfa0bf79d816bb02182162369d4a4
guangtao <gtrunsec@hardenedlinux.org>
2023-02-05 19:54:04 -0800

    Enable Quarto publisher and sync kernels
    - Enable Quarto publisher when `config.publishers.quarto.enable` is set to `true`
    - Add `rust`, `python`, and `r` fields to the example of the `quarto` option
    - Add a `quatroEnv` option with a `syncKernels` script
    - Move initialization of `config.build.passthru.kernels` to a separate function `getEnv`
    
    [nix/kernels/packages/modules/quatro.nix]
    - Enable the Quarto publisher when `config.publishers.quarto.enable` is set to `true`
    - Add `rust`, `python`, and `r` fields to the example of the `quarto` option
    - Add a `quatroEnv` option with a `syncKernels` script
    - Move `config.build.passthru.kernels` initialization to a separate function `getEnv`
    - Add a `lib.

--- nix/kernels/packages/modules/quatro.nix
+++ nix/kernels/packages/modules/quatro.nix
@@ -9,6 +9,7 @@
 }: let
   types = lib.types;
   getEnv = k: x: builtins.head config.build.passthru.kernels."${config.kernel.${k}.${x}.name}-jupyter-kernel".kernelInstance.argv;
+  cfg = config.publishers;
 in {
   options = {
     quatroEnv = lib.mkOption {
@@ -19,6 +20,11 @@ in {
       quarto = lib.mkOption {
         default = {};
         description = "Quarto publisher configuration";
+        example = ''
+          rust = "<name>"
+          python = "data-science";
+          r = "data-science";
+        '';
         type = types.submodule {
           options = {
             enable = lib.mkEnableOption (lib.mkDoc "whether to enable the quarto publisher");
@@ -34,32 +40,27 @@ in {
               description = "Python Kernel name";
               apply = getEnv "python";
             };
-            description = "quarto publisher configuration";
-            default = {};
-            example = ''
-              QUARTO_LANGUAGES = {
-                QUARTO_PYTHON = "python";
-              };
-            '';
           };
         };
       };
     };
   };
-  config = {
-    quatroEnv = inputs.cells.quarto.lib.mkQuarto {
-      text = let
-        syncKernels = lib.concatMapStringsSep "\n" (p: ''
-          rsync --chmod +rw -avzh ${p}/kernels/${p.kernelInstance.name} \
-          "$HOME"/.local/share/jupyter/kernels
-        '') (lib.attrValues config.build.passthru.kernels);
-      in ''
-        if [ ! -d "$HOME"/.local/share/jupyter/kernels ]; then
-            mkdir -p "$HOME"/.local/share/jupyter/kernels
-        fi
-        ${syncKernels}
-        quarto "$@"
-      '';
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.quarto.enable {
+      quatroEnv = inputs.cells.quarto.lib.mkQuarto {
+        text = let
+          syncKernels = lib.concatMapStringsSep "\n" (p: ''
+            rsync --chmod +rw -avzh ${p}/kernels/${p.kernelInstance.name} \
+            "$HOME"/.local/share/jupyter/kernels
+          '') (lib.attrValues config.build.passthru.kernels);
+        in ''
+          if [ ! -d "$HOME"/.local/share/jupyter/kernels ]; then
+              mkdir -p "$HOME"/.local/share/jupyter/kernels
+          fi
+          ${syncKernels}
+          quarto "$@"
+        '';
+      };
+    })
+  ];
 }
